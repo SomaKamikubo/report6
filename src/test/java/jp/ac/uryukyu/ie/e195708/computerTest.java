2be67819aee5e187f01b5e0e3795655ed1038430
Souma Kamikubo <e195708@ie.u-ryukyu.ac.jp>
2020-01-20 23:28:29 +0900

    ユニットテストの追加（できなかった。）

---
+++ src/test/java/jp/ac/uryukyu/ie/e195708/computerTest.java
@@ -0,0 +1,32 @@
+package jp.ac.uryukyu.ie.e195708;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class computerTest {
+    /**
+     * garbageCardに０が三つあるときgarbageCardを初期化するか確認。
+     */
+    @Test
+    void passCheck() {
+        /*ArrayList<Integer> garbageCard = new ArrayList<>();
+        for(int i = 0;i < 3;i++) {
+            garbageCard.add(0);
+        }
+        int coun = 0;
+        for (int s = 0; s< garbageCard.size();s++) {
+            int a = garbageCard.get(s);
+            if (a == 0) {
+                coun += 1;
+                if (coun == 3) {
+                    garbageCard.clear();
+                    System.out.println("全員がパスしました。");
+                }
+            }
+        }*/
+        assertEquals("全員がパスしました。",computer.passCheck());
+    }
+}
